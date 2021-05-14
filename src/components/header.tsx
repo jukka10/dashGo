6f7d9ace897470cbd38a650beee146c82df8af5c
jukka <jukka4996@gmail.com>
2021-05-14 15:34:24 -0300

    add user info in header Component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserLine } from "react-icons/ri";
 
 const Header = () => {
   return (
@@ -44,6 +44,34 @@ const Header = () => {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex alignItems="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Jukka Dev</Text>
+            <Text color="gray.300" fontSize="small">
+              jukka4996@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            src="https://avatars.githubusercontent.com/u/56303929?v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
