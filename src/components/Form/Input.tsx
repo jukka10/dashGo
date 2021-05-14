f0b2d65ebcac5db1d46f19076aa19607a910d2a2
jukka <jukka4996@gmail.com>
2021-05-14 15:06:15 -0300

    initial

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import {
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel hmltFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
