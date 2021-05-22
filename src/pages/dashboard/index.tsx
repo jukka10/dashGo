87121d8d6e107d43cd172331a6453db6d810c3b6
jukka <jukka4996@gmail.com>
2021-05-22 16:23:38 -0300

    dashboard respondive

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -68,7 +68,7 @@ const Dashboard = () => {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4" pb="4">
               Inscritos da semana
             </Text>
