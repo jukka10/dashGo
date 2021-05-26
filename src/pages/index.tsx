3bf8a1c764789fa421b0cc31e101be9c8dbeb0b6
jukka <jukka4996@gmail.com>
2021-05-26 20:58:24 -0300

    form authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,51 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
-import Head from "next/head";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import { Input } from "../components/Form/Input";
 
-export default function SignIn() {
-  return (
-    <>
-      <Head>
-        <title>Login | DashGo</title>
-      </Head>
-      <Flex w="100vw" h="100vh" align="center" justify="center">
-        <Flex
-          as="form"
-          w="100%"
-          maxW={360}
-          bg="gray.800"
-          p="8"
-          borderRadius={8}
-          flexDir="column"
-        >
-          <Stack spacing="4">
-            <Input name="email" type="email" label="email" />
-            <Input name="password" type="password" label="senha" />
-          </Stack>
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
-          <Button type="submit" mt="6" colorScheme="pink" size="lg">
-            Entrar
-          </Button>
-        </Flex>
+export default function SignIn() {
+  const { register, handleSubmit } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Input
+            name="email"
+            type="email"
+            label="email"
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="senha"
+            {...register("password")}
+          />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
       </Flex>
-    </>
+    </Flex>
   );
 }
